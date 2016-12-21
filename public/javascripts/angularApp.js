b26e900d2751b2ec8405c1e40eede4638ada23cf
aaronistheman <aarons.7007@gmail.com>
2016-12-21 11:34:45 -0800

    Added soon-to-be navigation bar
    As with much of the previous changes, the code for this was from the
    Thinkster tutorial cited in the readme. Also, the navigation bar doesn't
    yet look like a navigation bar, because I haven't set up Twitter Bootstrap
    yet.

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -144,3 +144,14 @@ function($scope, $state, auth){
     })
   };
 }]); // AuthCtrl controller
+
+
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth) {
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
